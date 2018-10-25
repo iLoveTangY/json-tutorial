b420a73fc505c31f49b146ff5afae98d42db5537
tang <ytang007@163.com>
2018-10-25 19:31:34 +0800

    完成01

---
+++ json-c/lept_json.h
@@ -0,0 +1,47 @@
+/*
+ * Ceated by tang
+ * */
+#ifndef LEPTJSON_TEST_LEPT_JSON_H
+#define LEPTJSON_TEST_LEPT_JSON_H
+
+/* json 的所有数据类型，共 7 种 */
+typedef enum
+{
+    LEPT_NULL,
+    LEPT_FALSE,
+    LEPT_TRUE,
+    LEPT_NUMBER,
+    LEPT_STRING,
+    LEPT_ARRAY,
+    LEPT_OBJECT
+} lept_type;
+
+/* 自定义类型，存储解析结果 */
+typedef struct
+{
+    lept_type type;
+} lept_value;
+
+/* 解析函数的返回值，表示各种状态 */
+enum
+{
+    LEPT_PARSE_OK = 0,          /* 成功解析 */
+    LEPT_PARSE_EXPECT_VALUE,    /* 需要一个value但是没有（只有空白） */
+    LEPT_PARSE_INVALID_VALUE,   /* 无效的value */
+    LEPT_PARSE_ROOT_NOT_SIGULAR /* 一个值之后还有其他无效字符 */
+};
+
+/* @desc:       解析指定的json字符串
+ * @param v:    解析结果
+ * @param json: 要解析的字符串，不应该修改它，因此为 const
+ * @return:     自定义枚举类型，表示解析函数执行完之后的状态
+ * */
+int lept_parse(lept_value* v, const char* json);
+
+/* @desc:    获取制定解析结果的类型
+ * @param v: 要获取类型的lept_value
+ * @return:  枚举值，得到的type
+ * */
+lept_type lept_get_type(const lept_value* v);
+
+#endif /* LEPTJSON_TEST_LEPT_JSON_H */
