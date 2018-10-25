4e8238878a068e33d88dda0dc11f0e174731bf7c
tang <ytang007@163.com>
2018-10-25 19:48:56 +0800

    add test

--- json-c/test.c
+++ json-c/test.c
@@ -47,11 +47,49 @@ static void test_parse_false()
     EXPECT_EQ_INT(LEPT_FALSE, lept_get_type(&v));
 }
 
+static void test_expect_value()
+{
+    lept_value v;
+
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, ""));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, " "));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+}
+
+static void test_parse_invalid_value()
+{
+    lept_value v;
+
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "nul"));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "?"));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+}
+
+static void test_parse_root_not_singular()
+{
+    lept_value v;
+
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SIGULAR, lept_parse(&v, "true true"));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+}
+
 static void test_parse()
 {
     test_parse_null();
     test_parse_false();
     test_parse_true();
+    test_expect_value();
+    test_parse_invalid_value();
+    test_parse_root_not_singular();
 }
 
 int main()
